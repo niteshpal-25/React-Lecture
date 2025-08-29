95fce32adb41a308a6eb57f376964e68514e008d
Nitesh Pal <86134916+niteshpal-25@users.noreply.github.com>
2025-08-29 22:43:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Welcome from "./Welcome";
-import ErrorMessage from "./ErrorMessage";
+import './App.css'
+import ItemForm from './components/ItemForm'
+import ItemList from './components/ItemList'
+import React, { useState } from 'react'
+
 function App() {
-  let isLoggedIn = true;
+  const [items, setItems] = useState([]);
+
+  const addItem=(text)=>{
+    setItems([...items, { id: Date.now(), text, view: false }]);
+  }
+
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const toggleView = (id) => {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, view: !item.view } : item
+      )
+    );
+  };
+
   return (
-    <>{isLoggedIn ? <Welcome></Welcome> : <ErrorMessage></ErrorMessage>}</>
-  );
+    <>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+        <h1 className="text-2xl font-bold mb-6">React Item Manager</h1>
+        <ItemForm onAdd={addItem}></ItemForm>
+        <ItemList items={items} onDelete={deleteItem} onToggleView={toggleView}></ItemList>
+      </div>
+    </>
+  )
 }
 
-//3.24.06
-
-export default App;
+export default App
